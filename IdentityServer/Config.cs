8e30ad7b2d38fde5080c0a5e3a10b583705ef339
ninad.more.github@gmail.com <ninad.more.github@gmail.com>
2022-12-17 13:20:57 +0530

    Add project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("PersonAPI", "PersonAPI")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "WebClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "PersonAPI" }
+            }
+        };
+}
